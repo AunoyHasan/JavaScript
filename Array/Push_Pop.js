2c1cf7a582988e4fffb2bc6c7961fccc18db7549
Aunoy <hasanmahmudul2457@gmail.com>
2022-08-13 12:38:54 +0600

    Push Pop

---
+++ Array/Push_Pop.js
@@ -0,0 +1,19 @@
+arr = ['aunoy', 'dil', 'rifat', 'antu'];
+console.log(arr);
+
+//inset at the last index using push
+arr.push('zishan');
+console.log(arr);
+
+//inset at the last index using push
+arr.push('riyad');
+console.log(arr);
+
+//inset at the last index using push
+arr.push('rakib');
+console.log(arr);
+
+//remove the last index element using pop
+arr.pop();
+console.log(arr);
+

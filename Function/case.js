e9c75369b14e1117e83352cab19bdcd9fa1579c3
Aunoy <hasanmahmudul2457@gmail.com>
2022-08-13 16:32:13 +0600

    Switch Case

---
+++ Function/case.js
@@ -0,0 +1,30 @@
+var color = 'red';
+
+switch(color){
+    case 'blue':
+        console.log("color is blue");
+        break;
+    
+    case 'red':
+        console.log("color is red");
+        break;
+
+    case 'white':
+        console.log("color is white");
+        break;
+        
+    case 'black':
+        console.log("color is black");
+        break;
+    
+    case 'yellow':
+        console.log("color is yellow");
+        break;    
+    case 'green':
+        console.log("color is green");
+        break;
+
+    default:
+        console.log("Invalid Color");
+        // break is optional for default
+}

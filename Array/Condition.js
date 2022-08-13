384a46e786f60f7838d18763f5457e982cc2fe3d
Aunoy <hasanmahmudul2457@gmail.com>
2022-08-13 15:53:46 +0600

    While Loop

---
+++ Array/Condition.js
@@ -0,0 +1,7 @@
+var isFoodReady = true;
+
+if(isFoodReady == true){
+    console.log("I will Eat");
+}else{
+    console.log("I will not wat");
+}
---
+++ Loop/while.js
@@ -0,0 +1,15 @@
+var arr = [3, 6, 9, 12, 15, 'karim', 'rahim'];
+
+var i = 0;
+while(i<arr.length){
+    console.log(arr[i]);
+    i += 1;
+}
+
+///odd number 1 to 100 using while loop
+var count = 0;
+var odd_count = 0;
+while(count<=100){
+    if(count%2 != 0)
+    count++;
+}

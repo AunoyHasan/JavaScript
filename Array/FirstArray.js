b809eb334403c28c64e790198d04d77ee8553ac4
Aunoy <hasanmahmudul2457@gmail.com>
2022-08-13 12:31:11 +0600

    Array Index Accessing

--- Array/FirstArray.js
+++ Array/FirstArray.js
@@ -5,3 +5,10 @@ var arr1 = [1, 2, 'Rahim'];
 console.log(arr1);
 var arr1_length = arr1.length;
 console.log(arr1_length);
+
+console.log(arr1[1]); /// print the value
+console.log(arr1.indexOf(1)); ///return the index 
+console.log(arr1.indexOf(154)); ///return the index 
+
+arr1[0] = 95;
+console.log(arr1);

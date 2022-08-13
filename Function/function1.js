fa389298af700ffb91f442570ab8e6e853e0b103
Aunoy <hasanmahmudul2457@gmail.com>
2022-08-13 16:16:19 +0600

    Function

---
+++ Function/function1.js
@@ -0,0 +1,12 @@
+function add1(a, b){
+    return a+b;
+}
+
+var res = add1(3, 6);
+console.log(res);
+
+function add(a, b){
+    console.log('Additio of ', a, 'and ', b, 'is', a+b);
+}
+
+add(4, 6);

4e7490318bba14d7234510fd2c5fac3c091a246e
Aunoy <hasanmahmudul2457@gmail.com>
2022-08-21 20:38:33 +0600

    Array Sorting

---
+++ ProblemSolving/ReverseArray.js
@@ -0,0 +1,15 @@
+let arr = [3, 8, 90, 87, 98, 45, -6, 2];
+
+console.log("Before Sorting The array Is: ");
+var i = 0;
+while(i<arr.length){
+    console.log(arr[i]);
+    i++;
+}
+
+console.log("After Sorting The array Is: ");
+var j = arr.length-1;
+while(j>=0){
+    console.log(arr[j]);
+    j--;
+}

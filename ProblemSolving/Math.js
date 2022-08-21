a85236daf532d3c0ff6a8d8a5e82a7ff3c7b23a8
Aunoy <hasanmahmudul2457@gmail.com>
2022-08-21 20:17:44 +0600

    math Problem

---
+++ ProblemSolving/Math.js
@@ -0,0 +1,16 @@
+const number1 = -4;
+const output1 = Math.abs(number1);
+console.log("Absolute number is: "+output1);
+
+const number2 = 2.989787;
+const output2 = Math.ceil(number2);
+console.log("Ceiling number is: "+output2);
+
+const number3 = 2.989787;
+const output3 = Math.round(number3);
+console.log("Round number is: "+output3);
+
+const randomNumber = Math.random(); //random numbers bettween 0 and 1
+console.log(randomNumber);
+const randomNumber2 = Math.random()*10; //random numbers bettween 0 and 10
+console.log(randomNumber2);
